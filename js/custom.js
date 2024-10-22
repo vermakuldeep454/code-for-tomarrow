1d1314c6bb1e8494caf3457ffaec442f2a1e44c0
vermakuldeep454 <vermakuldeep454@gmail.com>
2024-10-22 16:36:19 +0530

    second-commit

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,31 @@
-         // Age slider
-         var ageSlider = document.getElementById("ageRange");
-         var ageOutput = document.getElementById("ageOutput");
-         ageOutput.innerHTML = ageSlider.value;
-         
-         ageSlider.oninput = function() {
-           ageOutput.innerHTML = this.value;
-         }
-         
-         // Contribution slider
-         var contributionSlider = document.getElementById("contributionRange");
-         var contributionOutput = document.getElementById("contributionOutput");
-         contributionOutput.innerHTML = contributionSlider.value;
-         
-         contributionSlider.oninput = function() {
-           contributionOutput.innerHTML = this.value;
-         }
+// Age slider
+var ageSlider = document.getElementById("ageRange");
+var ageOutput = document.getElementById("ageOutput");
+ageOutput.innerHTML = ageSlider.value;
+
+// Function to change slider color
+function updateSliderBackground(slider) {
+    var value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+    slider.style.background = 'linear-gradient(to right, #5178e0 ' + value + '%, #e0e0e0 ' + value + '%)';
+}
+
+// Initial background setup for age slider
+updateSliderBackground(ageSlider);
+
+ageSlider.oninput = function() {
+    ageOutput.innerHTML = this.value;
+    updateSliderBackground(this);  // Update the background color as the slider moves
+}
+
+// Contribution slider
+var contributionSlider = document.getElementById("contributionRange");
+var contributionOutput = document.getElementById("contributionOutput");
+contributionOutput.innerHTML = contributionSlider.value;
+
+// Initial background setup for contribution slider
+updateSliderBackground(contributionSlider);
+
+contributionSlider.oninput = function() {
+    contributionOutput.innerHTML = this.value;
+    updateSliderBackground(this);  // Update the background color as the slider moves
+}
